90092a5baaf77ceb3dfb8c1f32cc663c75b7abe3
miha <miha890r@gmail.com>
2022-10-02 10:52:45 +0600

    Initial (create)
    - Create .Net Solution
    
    - Create Service blank template for Authorization
    
    - Add .gitignore file that contains records for VisualStudio work folders

---
+++ Services/Autorization/Autorization.API/Program.cs
@@ -0,0 +1,26 @@
+var builder = WebApplication.CreateBuilder(args);
+var services = builder.Services;
+
+// Add services to the container.
+services.AddMvc();
+services.AddEndpointsApiExplorer();
+
+// Add API services
+
+
+// Configure CORS Policy and Cookie
+services.AddCors(options => options.AddPolicy("CorsPolicy", policy =>
+{
+    policy.WithOrigins("http://localhost:8080").AllowAnyMethod().AllowAnyHeader().AllowCredentials();
+}));
+services.ConfigureApplicationCookie(options => {
+    options.Cookie.SameSite = SameSiteMode.None;
+});
+
+var app = builder.Build();
+
+app.UseHttpsRedirection();
+app.UseCors("CorsPolicy");
+app.MapControllers();
+
+app.Run();
